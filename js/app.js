c925041509496e44fb1e1d87eeb6c59a7fef5fbb
cloudnalu <68409311+cloudnalu@users.noreply.github.com>
2022-12-17 21:08:13 -0700

    working local storage

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ let imgOne = document.getElementById('img-one');
 let imgTwo = document.getElementById('img-two');
 let imgThree = document.getElementById('img-three');
 
-let showResultsBtn = document.getElementById('results-Btn');
 let resultsContainer = document.getElementById('results-container');
 
 let chartCanvas = document.getElementById('myChart');
@@ -74,32 +73,32 @@ function renderImg() {
 
 function renderChart() {
   // use the chart constructor, passing in the canvas element and the chart object of the data
-  
+
   let chartLabels = [];
   let votesArray = [];
   let viewsArray = [];
 
   for (let index = 0; index < ducksArray.length; index++) {
     chartLabels.push(ducksArray[index].name);
-    votesArray.push(ducksArray[index].votesCount)  
+    votesArray.push(ducksArray[index].votesCount)
     viewsArray.push(ducksArray[index].views)
   }
-  let chartObj = {
-    type: 'bar',
-    data: {
-      labels: chartLabels,
-      datasets: [{
-        label: '# of Votes',
-        data: votesArray,
-        borderWidth: 1
-      }, 
-      {
-        label: '# of Views',
-        data: viewsArray,
-        borderWidth: 1
-      }]
+
+  const dataForChart = {
+    labels: chartLabels,
+    datasets: [{
+      label: '# of Votes',
+      data: votesArray,
+      borderWidth: 0
     },
-    options: {
+    {
+      label: '# of Views',
+      data: viewsArray,
+      borderWidth: 0,
+    }],
+  };
+
+  const optionsObject = {
       indexAxis: 'y',
       scales: {
         y: {
@@ -108,82 +107,103 @@ function renderChart() {
       },
       responsive: true,
       maintainAspectRatio: true,
-      aspectRatio: 1|1,
-      plugins: {
-        legend: {
-          position: 'bottom',
-        }, 
-        title: {
-          display: true,
-          text: 'Results Chart'
-        }
+      aspectRatio: 1 | 1,
+    }
+  ;
+ 
+  const pluginsObject = {
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: true,
+        text: 'Results Chart'
+      },
+      customCanvasBackgroundColor: {
+        color: '#AAF1F7n',
       }
+    };
+  
 
-    },
+  const dataObject = {
+    type: 'bar',
+    data: dataForChart,
+    options: optionsObject,
+    plugins: pluginsObject,
   };
 
   // first argument is the canvas element
   // second argument is the chart object
 
-  new Chart(chartCanvas, chartObj);
+  new Chart(chartCanvas, dataObject);
 }
 
-// **** EVENT HANDLERS *****
+  // **** EVENT HANDLERS *****
 
-function handleClick(event) {
-  let imgClicked = event.target.title;
-  for (let index = 0; index < ducksArray.length; index++) {
-    if (imgClicked === ducksArray[index].name) {
-      ducksArray[index].votesCount++;
+  function handleClick(event) {
+    let imgClicked = event.target.title;
+    for (let index = 0; index < ducksArray.length; index++) {
+      if (imgClicked === ducksArray[index].name) {
+        ducksArray[index].votesCount++;
+      }
+    }
+
+    votesRound--;
+    renderImg();
+
+    if (votesRound === 0) {
+      imgContainer.removeEventListener('click', handleClick);
+      renderChart();
+
+      // **** Local Storage **** 
+
+     let stringifiedDucks = JSON.stringify(ducksArray);
+      localStorage.setItem('myDucks', stringifiedDucks);
+
+
     }
   }
-  votesRound--;
+
+
+  // function handleShowResults() {
+  //   if (votesRound === 0) {
+  //     renderChart();
+  //     showResultsBtn.removeEventListener('click', handleShowResults);
+  //   }
+  // }
+
+  // **** EXECUTABLE CODE *****
+
+
+let retrievedDucks = localStorage.getItem('myDucks');
+let parsedDucks = JSON.parse(retrievedDucks);
+
+  const bag = new Duck('bag');d
+  const banana = new Duck('banana');
+  const bathroom = new Duck('bathroom');
+  const boots = new Duck('boots');
+  const breakfast = new Duck('breakfast');
+  const bubblegum = new Duck('bubblegum');
+  const chair = new Duck('chair');
+  const cthulhu = new Duck('cthulhu');
+  const dogDuck = new Duck('dog-duck');
+  const dragon = new Duck('dragon');
+  const pen = new Duck('pen');
+  const petSweep = new Duck('pet-sweep');
+  const scissors = new Duck('scissors');
+  const shark = new Duck('shark');
+  const sweep = new Duck('sweep', 'png');
+  const tauntaun = new Duck('tauntaun');
+  const unicorn = new Duck('unicorn');
+  const waterCan = new Duck('water-can');
+  const wineGlass = new Duck('wine-glass');
+
+  ducksArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass);
+
+
+
+  // **** Render Images and Results Container ****
+
   renderImg();
-  if (votesRound === 0) {
-    imgContainer.removeEventListener('click', handleClick);
 
-
-
-  }
-}
-
-
-function handleShowResults() {
-  if (votesRound === 0) {
-    renderChart();
-    showResultsBtn.removeEventListener('click', handleShowResults);
-  }
-}
-
-// **** EXECUTABLE CODE *****
-
-const bag = new Duck('bag');
-const banana = new Duck('banana');
-const bathroom = new Duck('bathroom');
-const boots = new Duck('boots');
-const breakfast = new Duck('breakfast');
-const bubblegum = new Duck('bubblegum');
-const chair = new Duck('chair');
-const cthulhu = new Duck('cthulhu');
-const dogDuck = new Duck('dog-duck');
-const dragon = new Duck('dragon');
-const pen = new Duck('pen');
-const petSweep = new Duck('pet-sweep');
-const scissors = new Duck('scissors');
-const shark = new Duck('shark');
-const sweep = new Duck('sweep', 'png');
-const tauntaun = new Duck('tauntaun');
-const unicorn = new Duck('unicorn');
-const waterCan = new Duck('water-can');
-const wineGlass = new Duck('wine-glass');
-
-ducksArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass);
-
-
-
-// **** Render Images and Results Container ****
-
-renderImg();
-
-imgContainer.addEventListener('click', handleClick);
-showResultsBtn.addEventListener('click', handleShowResults);
+  imgContainer.addEventListener('click', handleClick);
